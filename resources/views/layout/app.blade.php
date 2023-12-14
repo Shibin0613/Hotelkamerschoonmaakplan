0c84e09cb022919914a3d228a583e0dceaccef00
ShibinPan <220617@edu.rocfriesepoort.nl>
2023-12-14 15:45:09 +0100

    login

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Load Bootstrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Load Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Load jQuery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- Load assets (Vite) -->
+    @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
+    <title>Hotelkamerschoonmaakplan</title>
+</head>
+
+<body>
+    <div class="page-container">
+        <div id="sidebar">
+            <div>
+                <div class="logo" id="logo">
+                    <img src="{{ asset('images/HSP-logo.png') }}" alt="Examinator Logo">
+                </div>
+                <ul>
+                    @if (Auth::user()->role === 1)
+                        <li><a class="link" href="/gebruikers" title="Gebruikers"><i class="fa-regular fa-user"></i></a></li>
+                        <li><a class="link" href="/templates" title="Templates"><i class="fa-regular fa-copy"></i></a></li>
+                    @endif
+                    <li><a class="link" href="/beoordelingen" title="Beoordelingen"><i class="fa-regular fa-file-lines"></i></a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a class="link" href="/profiel" title="Profiel"><i class="fa-regular fa-user-circle"></i></a></li>
+                    <li><a class="link" href="/logout" title="Uitloggen"><i class="fa-solid fa-arrow-right-from-bracket"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <main>
+            @yield('content')
+        </main>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+</html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,7 +7,7 @@
     <!-- Load Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
-    <title>Examinator - login</title>
+    <title>Hotelkamerschoonmaakplan - login</title>
 </head>
 
 <body>
@@ -23,8 +23,8 @@
         </div>
         <div class="login-form">
             <div class="title">
-                <img src="{{ asset('images/examinator-logo.svg') }}" alt="Examinator Logo">
-                <h1>Examinator</h1>
+                <img src="{{ asset('images/HSP-logo.png') }}" alt="Examinator Logo">
+                <h1>Hotelkamerschoonmaakplan</h1>
             </div>
             @if (Session::has('error'))
                 <div class="alert alert-danger" role="alert">
@@ -34,11 +34,11 @@
             <form action="{{ route('login') }}" method="POST">
                 @csrf
                 <div class="input">
-                    <label for="email">Email address</label>
+                    <label for="email">Gebruikersnaam</label>
                     <input type="email" name="email" id="email" placeholder="Vul hier je e-mailadres in...">
                 </div>
                 <div class="input">
-                    <label for="password">Password</label>
+                    <label for="password">Wachtwoord</label>
                     <input type="password" name="password" id="password" placeholder="Vul hier je wachtwoord in...">
                 </div>
                 <button>Login</button>
